82afb5698aa6a1cff15f7eee8c7642e3a5278670
GranFauno <p1xeler1239587@gmail.com>
2021-07-31 13:23:41 +0300

    add: everything

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function()
+{
+  const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -43,4 +45,65 @@ menuButton.addEventListener("click", function(){
   document
   .querySelector(".navbar-bottom")
   .classList.toggle("navbar-bottom_visible");
-})
+});
+
+  var modalButton=$('[data-toggle="modal"]');
+  var closeModalButton=$('.modal__close');
+  closeModalButton.on('click', closeModal);
+  
+  
+  function closeModal(event){
+    event.preventDefault();    
+    var modalOverlay=$(".modal__overlay");
+    var modalDialog=$(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  };  
+  modalButton.on('click', openModal);
+  function openModal(){
+    var targetModal=$(this).attr("data-href");
+      $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+      $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+    }
+
+    $(".form").each(function()
+    {
+      $(this).validate({
+        errorClass:"invalid",
+      rules:{
+        name:{
+          required: true,
+          minlength: 2
+        },
+        phone:{
+          required: true
+        },
+        email:{
+          required: true 
+        } 
+      },
+      messages: {
+        name: {
+          required:"Enter your name",
+          minleanght: jQuery.validator.format("At least {2} characters required!")          
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+          minlenght:  jQuery.validator.format("At least {2} characters required!")
+        },
+        phone:{
+        required: "Enter your phone",
+        minlenght: jQuery.validator.format("At least {2} characters required!")
+       }
+  }
+    });
+    }
+    )
+    AOS.init();
+    $('.phone').mask('+7 (000) 000-00-00');
+  });
+  
+  
+  
+
---
+++ send.php
@@ -0,0 +1,62 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+
+// Формирование самого письма
+$title = "Заголовок письма";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+<b>Email пользователя:</b> $email
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'message.sender142@gmail.com'; // Логин на почте
+    $mail->Password   = 'asiengr13'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('message.sender142@gmail.com', 'Саша Артемов'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('p1xeler1239587@gmail.com');  
+    // $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+
+    // Прикрипление файлов к письму
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
